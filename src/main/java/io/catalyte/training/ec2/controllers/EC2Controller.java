4aff4a89f4f603117c82703ba24e6fe86bdeddf7
Dan Reuther <dreuther@catalyte.io>
2020-04-14 16:45:23 -0700

    Allow all CORS

--- src/main/java/io/catalyte/training/ec2/controllers/EC2Controller.java
+++ src/main/java/io/catalyte/training/ec2/controllers/EC2Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ec2")
 public class EC2Controller {
